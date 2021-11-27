ade413a120d7d9d38b4dbda70ad79ea62977855d
ashahid91 <94854691+ashahid91@users.noreply.github.com>
2021-11-27 09:18:24 -0500

    Update DemoOne.java
    added two variables

--- src/one/DemoOne.java
+++ src/one/DemoOne.java
@@ -5,5 +5,8 @@ public class DemoOne {
 			System.out.println("This is not private method");
 			System.out.println("Random change");
 		//this line of code is added in github
+		
+		int a =10;
+		int b = 20;
 	}
 }
